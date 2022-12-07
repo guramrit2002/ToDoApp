6cad0df9082e070ee6023ada784df70755621453
Amrit5s2002 <guramrit1066@gmail.com>
2022-12-08 01:32:48 +0530

    qq

--- to_do_list/todolist/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns = [
-    path('',views.getNotes),
-    path('notes/<int:pk>',views.getNote),
-    path('notes/create',views.createnote),
-    path('notes/<int:pk>/update',views.updatenote),
-    path('notes/<int:pk>/delete',views.deletenote),
-]
